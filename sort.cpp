8e18ea5ae08ae48596cbc939dd0d028d340fcbb8
Austin White <austingym@gmail.com>
2014-10-26 20:06:53 -0700

    Updated the sort.cpp file to facilitate faster scatterplot charting

--- sort.cpp
+++ sort.cpp
@@ -9,14 +9,6 @@
 
 using namespace std;
 
-void replace_this_with_a_real_algorithm() {
-  // waste time...
-  const int N = 1 << 15;
-  for (int i = 0; i < N; ++i)
-    for (int j = 0; j < N; ++j)
-      ;
-}
-
 vector<string> ip_selection_sort(vector<string> L) {
     int n = L.size();
 
@@ -42,6 +34,60 @@ void print_first_lines(vector<string> inp, int n) {
         cout << (i + 1) << ". " << inp[i] << endl;
 }
 
+void run(int n, string filename, bool preassigned = false) {
+    // 1. Load input file
+    ifstream f(filename);
+
+    vector<string> L;
+
+    int i = 0;
+    string line;
+
+    while (getline(f, line)) {
+        if (i == n) break;
+
+        L.push_back(line);
+
+        ++i;
+    }
+
+    // Be a good citizen
+    f.close();
+
+    // 3. Print the first 10 words
+    if ( ! preassigned)
+        print_first_lines(L, 10);
+
+    if ( ! preassigned)
+        cout << "In place selection sort..." << endl;
+
+    // Start the timer
+    auto start = chrono::high_resolution_clock::now();
+
+    // 4. Sort the first n words
+    vector<string> sorted = ip_selection_sort(L);
+
+    // Stop the timer
+    auto end = chrono::high_resolution_clock::now();
+
+    // 5. Measure running time
+    int microseconds = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    double seconds = microseconds / 1E6;
+
+    // 6. Print the first 10 words in sorted sequence
+    if ( ! preassigned) {
+        cout << "Sorted:" << endl;
+        print_first_lines(sorted, 10);
+    }
+
+    // 7. Print the elapsed time
+    if ( ! preassigned) {
+        cout << "Elapsed time for n = " << n << ": " << seconds << " seconds" << endl;
+    } else {
+        cout << seconds << endl;
+    }
+}
+
 int main(int argc, char** argv) {
 
     string filename;
@@ -54,54 +100,26 @@ int main(int argc, char** argv) {
     }
 
     try {
-        // 1. Load input file
-        ifstream f(filename);
+
+        bool preassigned = false;
+        int n;
 
         // 2. Specify n to use
-        int n;
-        cout << "For what n? ";
-        cin >> n;
-
-        vector<string> L;
-
-        int i = 0;
-        string line;
-
-        while (getline(f, line)) {
-            if (i == n) break;
-
-            L.push_back(line);
-
-            ++i;
+        if (argc > 1) {
+            preassigned = true;
+            n = atoi(argv[1]);
+        } else {
+            cout << "For what n? ";
+            cin >> n;
         }
 
-        // Be a good citizen
-        f.close();
-
-        // 3. Print the first 10 words
-        print_first_lines(L, 10);
-
-        cout << "In place selection sort..." << endl;
-
-        // Start the timer
-        auto start = chrono::high_resolution_clock::now();
-
-        // 4. Sort the first n words
-        vector<string> sorted = ip_selection_sort(L);
-
-        // Stop the timer
-        auto end = chrono::high_resolution_clock::now();
-
-        // 5. Measure running time
-        int microseconds = chrono::duration_cast<chrono::microseconds>(end - start).count();
-        double seconds = microseconds / 1E6;
-
-        // 6. Print the first 10 words in sorted sequence
-        cout << "Sorted:" << endl;
-        print_first_lines(sorted, 10);
-
-        // 7. Print the elapsed time
-        cout << "Elapsed time: " << seconds << " seconds" << endl;
+        if (preassigned) {
+            for (int i = 1; i <= n; ++i) {
+                run(i, filename, preassigned);
+            }
+        } else {
+            run(n, filename);
+        }
 
     } catch (ifstream::failure e) {
         cerr << "Error opening file";
